067de55873166148c05467b2d6031a5c8edaace6
Peter Leite <peterleite.dev@gmail.com>
2021-09-05 10:22:00 -0300

    added js para alterar as imgs no input select

--- js/app.js
+++ js/app.js
@@ -6,14 +6,15 @@ const comentario = document.querySelector("textarea")
 const botaoEnviar = document.getElementById("botaoEnviar")
 
 //função para verificação se está vazio
-botaoEnviar.addEventListener("click", (event)=>{//event recebe o click
+botaoEnviar.addEventListener("click", (event) => {//event recebe o click
     inputs.forEach(element => {
-        if(element.value == ""){
+        if (element.value == "") {
             event.preventDefault()
             // alert("Preencha corretamente") decidir o que vamos fazer, se vai ser div, texto, etc...
             //pode ser um createelement, appendchild e depois um p
         }
-})})
+    })
+})
 
 //função para limitar a quantidade de caracteres
 // comentario.addEventListener("keyup", (event)=>{
@@ -28,3 +29,24 @@ botaoEnviar.addEventListener("click", (event)=>{//event recebe o click
 // console.log(Date().toString())
 
 //adicionar contador regressivo na textarea
+
+//PERFIL
+const perfil = document.getElementById('perfil');
+const userImg = document.getElementById('userImg')
+
+//TODO: CONSERTAR O ERRO QUE CRUIA VARIOS INPUTS QUANDO 'OUTROS' é selecionado
+//TODO: Ajustar o ALT
+perfil.addEventListener('change', event => {
+    let element = event.target.value
+
+    if (element == 'outro') {
+        // const novoPerfil = document.createElement('input')
+        //     novoPerfil.placeholder = 'Digite o perfil'
+        //     novoPerfil.id = 'novoPerfil'
+        //     document.getElementById('perfilContainer').appendChild(novoPerfil)
+    }
+    else if (element != '') {
+        userImg.src = `/assets/${element}.jpg`
+        userImg.alt = `Fotografia de uma viagem à ${element}`
+    }
+})
