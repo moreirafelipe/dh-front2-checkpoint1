72741288697c1e9351592913f26924eeb229eed5
FlipeFrontDev <felipe.saint@gmail.com>
2021-09-08 20:08:55 -0300

    Finalizando funcao localStorage

--- js/app.js
+++ js/app.js
@@ -11,43 +11,48 @@ let divCards = document.getElementById("cards")
 //selecionar o numero de caracteres da textarea
 let divCaracteres = document.getElementById("numCaracteres");
 
-let arrayObjetos = [];
-
 /* INICIO: ATUALIZEI EM 07/09 - FELIPE */
 //propriedade para executar quaisquer funções ao carregar a página
 window.onload = _ => {
 
     defineCalendario();
+    mantemCards();
+
+}
+
+//Funções gerais
+
+//Função para recuperar dados do usuario ao fechar página
+const mantemCards = () => {
 
     let obj = JSON.parse(localStorage.getItem('card'));
 
     obj.forEach((element) => {
 
-    const card = document.createElement("div");
-    card.setAttribute("class", "cards")
+        const card = document.createElement("div");
+        card.setAttribute("class", "cards")
 
-    const title = document.createElement("h2");
-    title.setAttribute("class", "tituloCard")
-    title.innerHTML = element.titulo
+        const title = document.createElement("h2");
+        title.setAttribute("class", "tituloCard")
+        title.innerHTML = element.titulo
 
-    const img = document.createElement("img")
-    img.setAttribute("src", element.imagem)
-    
+        const img = document.createElement("img")
+        img.setAttribute("src", element.imagem)
+        
 
-    const text = document.createElement("p");
-    text.setAttribute("class", "paragCard")
-    text.innerHTML = element.comentario
-    
-    card.appendChild(title)
-    card.appendChild(img)
-    card.appendChild(text)
-    
-    divCards.appendChild(card)
+        const text = document.createElement("p");
+        text.setAttribute("class", "paragCard")
+        text.innerHTML = element.comentario
+        
+        card.appendChild(title)
+        card.appendChild(img)
+        card.appendChild(text)
+        
+        divCards.appendChild(card)
 
-    });
+    });    
 }
 
-//Funções gerais
 //Função para criar calendário e ajustar sua data atual
 const defineCalendario = _ => {
 
@@ -138,6 +143,7 @@ perfil.addEventListener('change', event => {
 })
 
 /* Criar cards - 07/09 - Dâmares */
+/* ATUALIZEI - 08/09 - FELIPE - localStorage */
 botaoEnviar.addEventListener('click', function(event){
     event.preventDefault();
 
@@ -161,21 +167,31 @@ botaoEnviar.addEventListener('click', function(event){
     
     divCards.appendChild(card)
 
-    let obj = {titulo: perfil.value, imagem: userImg.src, comentario: comentario.value};
+    /* Definindo array local temporário para ser inicicializado a cada inserção de card */
+    let arrayObjetos = [];
+    /* Definindo objeto com dados do card atual */
+    let newObj = {titulo: perfil.value, imagem: userImg.src, comentario: comentario.value};
 
+    /* Verifica se o localStorage esta vazio para decidir se deve recuperar dados do localStorage */
     if(localStorage.length == 0) {
+        arrayObjetos.push(newObj)
 
-        arrayObjetos.push(obj);
-
-    } else {
+    } else {    
+        /* Recupera dados dos cards antigos do localStorage para renderizar na tela e insere cada um no array com forEach*/ 
         let getObj = JSON.parse(localStorage.getItem('card'));
-        arrayObjetos.push(getObj);
-        arrayObjetos.push(obj);
+        getObj.forEach(element => {
+            arrayObjetos.push(element);
+        });
+
+        /* Insere dados do novo card no array de objetos temporario */
+        arrayObjetos.push(newObj)
+        /* Apaga localStorage para receber array atualizado com cards antigos e novos */
+        localStorage.removeItem('card');
     }
 
-    /* console.log(arrayObjetos) */
-
+    /* Insere o array temporario convertido em JSON no localStorage */
     localStorage.setItem('card', JSON.stringify(arrayObjetos));
+    /* FIM ATUALIZEI - 08/09 - FELIPE - localStorage */
 
     //INICIO: ATUALIZEI EM 08/09 - DUYLLYAN
     //zerando o contador do textarea
@@ -183,3 +199,5 @@ botaoEnviar.addEventListener('click', function(event){
     divCaracteres.innerHTML = comentario.value.length + "/" + 150;
     //FIM: ATUALIZEI EM 08/09 - DUYLLYAN
 })
+
+console.log(this.localStorage.length)
